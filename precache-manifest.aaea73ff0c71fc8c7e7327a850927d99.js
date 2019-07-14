1332ccf919167e9eb231cb49a07ce0315bbaf0c0
Muheeb Olakunle <olakunlemuheeb@gmail.com>
2019-07-14 17:12:36 +0100

    Updates

---
+++ precache-manifest.aaea73ff0c71fc8c7e7327a850927d99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e24c844100aea727c1214b6fc17398ee",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "bf4e746e13228c2bd08b",
+    "url": "/contactmanager/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "7e21d78edb921173299c",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "bf4e746e13228c2bd08b",
+    "url": "/contactmanager/static/js/2.aaeba3a9.chunk.js"
+  },
+  {
+    "revision": "7e21d78edb921173299c",
+    "url": "/contactmanager/static/js/main.52de1cdb.chunk.js"
+  },
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  }
+]);
